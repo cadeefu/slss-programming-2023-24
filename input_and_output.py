26576288f2778975826d324088369424e13221a9
Cadee Fu <cadeefu@hotmail.com>
2023-09-19 14:55:53 -0700

    Added notes to NLP and added Python file

---
+++ input_and_output.py
@@ -0,0 +1,10 @@
+#Input and Output
+# Author: Cadee Fu
+# 19 September 2023
+
+# Output a quote from someone famous
+print("Git is Google Drive on caffeiene.")
+
+print("I am so tired of school")
+
+print("Lynn Kim and Kyle Li are my friends.")
